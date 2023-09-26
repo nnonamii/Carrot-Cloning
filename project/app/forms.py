87f51e5781c501c721f20684a67c7e20f1f0a65b
kys <crosstosaw@naver.com>
2023-09-26 13:20:52 +0900

    로그인, 로그아웃, 회원가입 기능 추가

---
+++ project/app/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+class CustomLoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '아이디를 입력해주세요', 'class': 'login-input'}),
+        label='아이디',)
+    
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호',)
+    
+
+class CustomRegistrationForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '아이디를 입력해주세요', 'class': 'login-input'}),
+        label='아이디',
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호', 
+    )
+    check_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 다시 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호 확인',
+    )
