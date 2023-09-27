98dfbd9c84b92e9de344296d2021185ceb04a797
wooookim <jhr01175@naver.com>
2023-09-27 17:04:16 +0900

    Add : 중고차 직거래

--- project/app/models.py
+++ project/app/models.py
@@ -43,23 +43,38 @@ class Chat(models.Model):
 
 
 class Job(models.Model):
-    title               = models.CharField(verbose_name='제목',max_length=200)
-    price               = models.IntegerField(verbose_name='시급')
-    description         = models.TextField(verbose_name='설명')
-    location            = models.CharField(verbose_name='지역',max_length=100)
-    images              = models.ImageField(verbose_name='이미지',upload_to='post_images/') 
-    user                = models.ForeignKey(User, on_delete=models.CASCADE, null=True, to_field='username')
-    working_days        = models.CharField(verbose_name='근무 요일',max_length=100)
-    working_time        = models.CharField(verbose_name='근무 시간',max_length=100)
-    created_at          = models.DateTimeField(verbose_name='작성일자',auto_now_add=True, null=True) 
+    title = models.CharField(verbose_name="제목", max_length=200)
+    price = models.IntegerField(verbose_name="시급")
+    description = models.TextField(verbose_name="설명")
+    location = models.CharField(verbose_name="지역", max_length=100)
+    images = models.ImageField(verbose_name="이미지", upload_to="post_images/")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, to_field="username")
+    working_days = models.CharField(verbose_name="근무 요일", max_length=100)
+    working_time = models.CharField(verbose_name="근무 시간", max_length=100)
+    created_at = models.DateTimeField(verbose_name="작성일자", auto_now_add=True, null=True)
 
-    product_sold        = models.CharField(verbose_name='마감여부',max_length=1, default='N')
+    product_sold = models.CharField(verbose_name="마감여부", max_length=1, default="N")
 
-    view_num            = models.PositiveIntegerField(verbose_name='조회수',default=0)
-    chat_num            = models.PositiveIntegerField(verbose_name='채팅수',default=0)
+    view_num = models.PositiveIntegerField(verbose_name="조회수", default=0)
+    chat_num = models.PositiveIntegerField(verbose_name="채팅수", default=0)
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
+
+
+class Oldcar(models.Model):
+    title = models.CharField(verbose_name="제목", max_length=200)
+    price = models.IntegerField(verbose_name="가격")
+    car_info = models.TextField(verbose_name="차량 정보")
+    insurance_history = models.TextField(verbose_name="보험 이력")
+    description = models.TextField(verbose_name="설명")
+    location = models.CharField(verbose_name="지역", max_length=100)
+    images = models.ImageField(verbose_name="이미지", upload_to="oldcar_images/")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, to_field="username")
+    created_at = models.DateTimeField(verbose_name="작성일자", auto_now_add=True, null=True)
+    product_sold = models.CharField(verbose_name="마감여부", max_length=1, default="N")
+    view_num = models.PositiveIntegerField(verbose_name="조회수", default=0)
+    chat_num = models.PositiveIntegerField(verbose_name="채팅수", default=0)
