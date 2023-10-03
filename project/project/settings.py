f64f716bfc363cdcb739bd1b52a26f294f3cdd58
wooookim <jhr01175@naver.com>
2023-10-03 14:39:28 +0900

    Add : 중고차 수정/삭제

--- project/project/settings.py
+++ project/project/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "channels",
     "channels_redis",
     "app",
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = "project.urls"
@@ -102,7 +100,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-INTERNAL_IPS = ('127.0.0.1')
+INTERNAL_IPS = "127.0.0.1"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
